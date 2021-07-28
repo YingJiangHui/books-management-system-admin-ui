1ba3930d8164a52b784a5e38fa0304b02d8efb4f
Jh <473380917@qq.com>
2021-07-28 08:36:38 +0800

    build: 安装依赖
    @umijs/use-request
    @ant-design/pro-card

--- src/access.ts
+++ src/access.ts
@@ -3,12 +3,12 @@
  * */
 enum Roles {
   Admin = 'admin',
-  User = 'user'
+  User = 'user',
 }
 export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
   const { currentUser } = initialState || {};
   return {
-    canAdmin: currentUser && currentUser.roles.includes(Roles.Admin),
-    canUser: currentUser && currentUser.roles.includes(Roles.User),
+    canAdmin: currentUser && currentUser.roles?.includes(Roles.Admin),
+    canUser: currentUser && currentUser.roles?.includes(Roles.User),
   };
 }
--- src/services/API.d.ts
+++ src/services/API.d.ts
@@ -1,82 +1,81 @@
 declare namespace API {
-
   type SignParams = {
-    username: string,
-    password: string,
-    confirmPassword: string,
-    nationId: number,
-    email: string,
-  }
+    username: string;
+    password: string;
+    confirmPassword: string;
+    nationId: number;
+    email: string;
+  };
 
   type LoginParams = {
-    username: string,
-    password: string,
-  }
+    username: string;
+    password: string;
+  };
 
-  type LoginResult = {
-    statusCode: number,
-    message: string
-  } | {}
+  type LoginResult =
+    | {
+        statusCode: number;
+        message: string;
+      }
+    | {};
 
   type CurrentUser = {
-    username: string,
-    roles: ('user' | 'admin')[],
-    email: string,
-    nation: string,
-  }
+    username: string;
+    roles?: ('user' | 'admin')[];
+    email?: string;
+    nation?: string;
+  };
 
   type Nation = {
-    name: string,
-    id: number
-  }
+    name: string;
+    id: number;
+  };
 
   type ErrorData = {
-    error: string
-    message: {field: string, subErrors: string[]}[]
-    statusCode: number
-  }
+    error: string;
+    message: { field: string; subErrors: string[] }[];
+    statusCode: number;
+  };
 
   interface Category {
-    id: number
-    name: string
+    id: number;
+    name: string;
   }
 
   interface Publisher {
-    id: number
-    name: string
+    id: number;
+    name: string;
   }
 
   type BookBaseInfo = {
-    name: string
-    imagePath?: string
-    description?: string
-    author: string
-    publisher: Publisher
-    categories: Category[]
-  }
+    name: string;
+    imagePath?: string;
+    description?: string;
+    author: string;
+    publisher: Publisher;
+    categories: Category[];
+  };
 
-  interface Book extends BookBaseInfo{
-    id: number
-    publicationDate?: string
+  interface Book extends BookBaseInfo {
+    id: number;
+    publicationDate?: string;
   }
 
   declare namespace Book {
-    type AddParams = BookField
+    type AddParams = BookField;
 
-    interface UpdateParams extends Partial<BookBaseInfo>{
-      id: number,
+    interface UpdateParams extends Partial<BookBaseInfo> {
+      id: number;
     }
 
     interface DeleteParams {
-      id: number
+      id: number;
     }
 
-    interface GetParams{
-      id?: number
-      searchText?: string
-      categoryId?: number,
+    interface GetParams {
+      id?: number;
+      searchText?: string;
+      categoryId?: number;
     }
   }
 }
-
-
