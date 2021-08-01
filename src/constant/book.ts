ef7a2fe3b37c1837c8378730a2e6d10e47f29c24
Jh <473380917@qq.com>
2021-08-01 11:25:10 +0800

    refactor: 添加新的状态 REFUSE

--- src/constant/book.ts
+++ src/constant/book.ts
@@ -1,13 +1,14 @@
 export const bookStatusToTextMap = {
-  APPLIED:'已申请',
-  RESERVED:'已预约',
-  BORROWED:'已借取',
-  LOST:'遗失'
-}
+  APPLIED: '已申请',
+  RESERVED: '已预约',
+  BORROWED: '已借取',
+  REFUSE: '以拒绝',
+  LOST: '遗失',
+};
 export const bookStatusToStyleMap = {
-  APPLIED:'#2db7f5',
-  RESERVED:'#fb982d',
-  BORROWED:'#87d068',
-  LOST:'#d9d9d9'
-}
-
+  APPLIED: '#2db7f5',
+  RESERVED: '#fb982d',
+  BORROWED: '#87d068',
+  REFUSE: '#000',
+  LOST: '#d9d9d9',
+};
